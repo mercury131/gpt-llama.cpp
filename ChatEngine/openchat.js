2718a7381406c6e1dd1b78d1795a5c290c305193
mercury131 <34038246+mercury131@users.noreply.github.com>
2023-12-13 17:30:49 +0300

    Update openchat.js

--- ChatEngine/openchat.js
+++ ChatEngine/openchat.js
@@ -11,9 +11,16 @@ export class Openchat extends ChatEngine {
 			// roleMap: { assistant: 'ASSISTANT', user: 'USER', system: 'SYSTEM' },
 			//roleMap: { user: 'GPT4 User: ', assistant: '<|end_of_turn|>GPT4 Assistant:',},
 			roleMap: { assistant: '<|end_of_turn|>GPT4 Assistant:', user: 'USER', system: 'GPT4 User: ' },
-			stopPrompts: ['[end of text]'],
+			//stopPrompts: ['[end of text]'],
 			// Need few-shot prompting or else it goes off the rails and generates chinese in 3B
 			defaultMsgs:[],
 		});
+		this.stopPrompts = [
+			...this.stopPrompts,
+			'</s>',
+			'\n</s>',
+			'\n</s',
+			'[end of text]',
+		];		
 	}
 }
