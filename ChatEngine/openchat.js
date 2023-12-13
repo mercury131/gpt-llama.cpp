891922d147c865852ea3354c5064e79847f1a750
mercury131 <34038246+mercury131@users.noreply.github.com>
2023-12-13 16:13:43 +0300

    Create openchat.js

---
+++ ChatEngine/openchat.js
@@ -0,0 +1,20 @@
+import { ChatEngine } from './index.js';
+
+/**
+ * This works for the following models (but is not limited to):
+ * WizardLM
+ */
+export class WizardLM extends ChatEngine {
+	constructor() {
+		super({
+			roleMap: { assistant: 'ASSISTANT', user: 'USER', system: 'SYSTEM' },
+			instructions:
+				"GPT4 User: ",
+		});
+		this.stopPrompts = [
+			...this.stopPrompts,
+			'</s>',
+			'<|end_of_turn|>GPT4 Assistant:',
+		];
+	}
+}
