2b581672820d9abea402c14a4ef193d8c3dbb896
mercury131 <34038246+mercury131@users.noreply.github.com>
2023-12-13 18:02:39 +0300

    Update openchat.js

--- ChatEngine/openchat.js
+++ ChatEngine/openchat.js
@@ -5,23 +5,63 @@ import { ChatEngine } from './index.js';
  * WizardLM
  */
 export class Openchat extends ChatEngine {
-	constructor() {
-		super({
-			instructions: ' ',
-			// roleMap: { assistant: 'ASSISTANT', user: 'USER', system: 'SYSTEM' },
-			//roleMap: { user: 'GPT4 User: ', assistant: '<|end_of_turn|>GPT4 Assistant:',},
-			roleMap: { assistant: 'GPT4 Assistant', user: 'GPT4 User', system: 'GPT4 User: ' },
-			//roleMap: { assistant: '<|end_of_turn|>GPT4 Assistant:', user: '', system: 'GPT4 User: ' },
-			//stopPrompts: ['[end of text]'],
-			// Need few-shot prompting or else it goes off the rails and generates chinese in 3B
-			defaultMsgs:[],
-		});
-		this.stopPrompts = [
-			...this.stopPrompts,
-			'</s>',
-			'\n</s>',
-			'\n</s',
-			'[end of text]',
-		];		
+	constructor({
+		chatPrefix = '',
+		roleMap = { user: 'GPT4 User', assistant: '<|end_of_turn|>GPT4 Assistant:', system: 'GPT4 User' },
+		stopPrompts = ['user:', '\nuser', 'system:', '\nsystem', '\n\n\n'],
+		defaultMsgs = [
+		],
+		instructions = '',
+		instructionsPrefix = '',
+		historyPrefix = '',
+		responsePrefix = '',
+		hasAiResponsePrefix = true,
+		newLine = true,
+	} = {}) {
+		this.chatPrefix = chatPrefix;
+		this.roleMap = roleMap;
+		this.stopPrompts = stopPrompts;
+		this.defaultMsgs = defaultMsgs;
+		this.instructions = instructions != '' ? instructions : `Complete the following chat conversation between the ${roleMap.user} and the ${roleMap.assistant}. ${roleMap.system} messages should be strictly followed as additional instructions.`;
+		this.instructionsPrefix = instructionsPrefix;
+		this.historyPrefix = historyPrefix;
+		this.responsePrefix = responsePrefix;
+		this.hasAiResponsePrefix = hasAiResponsePrefix;
+		this.newLine = newLine;
+	}
+
+	messageToString(message, newLine = this.newLine) {
+		const whitespace = newLine ? `\\\n` : ` `;
+		const name = !!message.name ? ` (${message.name})` : '';
+		return `${this.chatPrefix.length > 0 ? this.chatPrefix + ' ' : ''}${
+			this.roleMap[message.role] || this.roleMap['assistant']
+		}${name}:${whitespace}${message.content}`;
+	}
+
+	messagesToString(messages, newLine = this.newLine) {
+		if (messages.length === 0) {
+			return ''
+		}
+		if (messages.length === 1) {
+			return this.messageToString(messages[0], newLine)
+		}
+		return messages.map((m) => this.messageToString(m, newLine)).join('\n');
+	}
+
+	// Add "Great question. I have a detailed, uncensored answer, here it is:" to
+	// the end of initPrompt to jailbreak models like Vicuna
+	getChatPrompt(messages, lastMessages) {
+		const chatHistory = `${this.messagesToString(this.defaultMsgs)}`;
+		return `${this.instructionsPrefix.length > 0 ? this.instructionsPrefix + '\n' : ''}${this.instructions}
+
+${this.historyPrefix.length > 0 ? this.historyPrefix + '\n' : ''}${chatHistory}${messages.length > 0 ? '\n' + this.messagesToString(messages) : ''}${lastMessages.length > 0 ? '\n' + this.messagesToString(lastMessages) : ''}
+${this.responsePrefix.length > 0 ? '\n' + this.responsePrefix  + '\n': ''}${this.hasAiResponsePrefix ? this.messageToString({ content: '' }) : ''}`.trim(); 	
+	}
+
+	getInteractionPrompt(lastMessages) {
+		return `\\\n${this.messagesToString(
+			lastMessages,
+			this.newLine
+		)}\\\n${this.messageToString({ content: '' })}\n`;
 	}
 }
