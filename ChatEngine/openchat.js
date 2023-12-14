88ad0aff1a7511e4fc95e6f826bbf5c6344bc5e8
mercury131 <34038246+mercury131@users.noreply.github.com>
2023-12-14 12:23:44 +0300

    Update openchat.js

--- ChatEngine/openchat.js
+++ ChatEngine/openchat.js
@@ -7,7 +7,7 @@ import { ChatEngine } from './index.js';
 export class Openchat extends ChatEngine {
 	constructor() {
 		super({
-			roleMap: { assistant: '<|end_of_turn|>GPT4 Assistant:', user: 'GPT4 User: ', system: 'SYSTEM' },
+			roleMap: { assistant: '<|end_of_turn|>GPT4 Assistant:', user: 'GPT4 User: ', system: '' },
 			defaultMsgs:[],			
 			instructions:
 				"GPT4 User: ",
